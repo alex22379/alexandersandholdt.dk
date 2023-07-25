4bf114eaef248e6e46f608b3211d8cb4a9eca68f
Alexander <alexanderhis2201@gmail.com>
2023-07-25 10:36:10 +0200

    Added lang-slider

--- scripts/copy-mail.js
+++ scripts/copy-mail.js
@@ -1,13 +1,15 @@
-const copyMail = document.querySelector('.copy-mail');
-const mailInput = copyMail.querySelector('input');
-const copyButton = copyMail.querySelector('button');
+window.onload = () => {
+  const copyMail = document.querySelector('.copy-mail');
+  const mailInput = copyMail.querySelector('input');
+  const copyButton = copyMail.querySelector('button');
 
-copyButton.addEventListener('click', (e) => {
-  mailInput.select();
-  document.execCommand('copy');
-  copyMail.classList.add('copied');
-  window.getSelection().removeAllRanges();
-  setTimeout(() => {
-    copyMail.classList.remove('copied');
-  }, 2500);
-});
+  copyButton.addEventListener('click', (e) => {
+    mailInput.select();
+    document.execCommand('copy');
+    copyMail.classList.add('copied');
+    window.getSelection().removeAllRanges();
+    setTimeout(() => {
+      copyMail.classList.remove('copied');
+    }, 2500);
+  });
+};
