7057fd4423a48cb521e1c3a29f9097f4d5d1fd39
Alexander <alexanderhis2201@gmail.com>
2023-09-16 21:06:53 +0200

    Added astro-purgecss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import i18n from 'astro-i18n';
 import { defineConfig } from 'astro/config';
 
+import purgecss from 'astro-purgecss';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [i18n()],
+  integrations: [i18n(), purgecss()],
   site: 'https://www.alexandersandholdt.dk/',
   redirects: {
     '/da': '/',
