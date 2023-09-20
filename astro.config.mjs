f4f97d41a9b280c1b0e0c55d31e68db9876429ce
Alexander <alexanderhis2201@gmail.com>
2023-09-20 20:57:37 +0200

    commit of the day

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import i18n from 'astro-i18n';
 import { defineConfig } from 'astro/config';
-
 import purgecss from 'astro-purgecss';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [i18n(), purgecss()],
-  site: 'https://www.alexandersandholdt.dk/',
+  integrations: [i18n(), purgecss(), sitemap()],
+  site: 'https://www.alexandersandholdt.dk',
   redirects: {
     '/da': '/',
   },
