54baf90454f57385baa8c1abe819145e75271b71
Alexander <alexanderhis2201@gmail.com>
2023-09-23 00:35:27 +0200

    cozy commit 2

---
+++ src/scripts/isTouchEnabled.js
@@ -0,0 +1,9 @@
+// from https://www.geeksforgeeks.org/how-to-detect-touch-screen-device-using-javascript/
+
+export default function isTouchEnabled() {
+  return (
+    'ontouchstart' in window ||
+    navigator.maxTouchPoints > 0 ||
+    navigator.msMaxTouchPoints > 0
+  );
+}
