8fa7b5b9bfe0a266ff2899c56124a1054c3b11eb
Alexander <alexanderhis2201@gmail.com>
2023-10-20 19:04:59 +0200

    cotd

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,3 +1,34 @@
-import { z, defineCollection } from 'astro:content';
+import { z, reference, defineCollection } from 'astro:content';
 
-export const collections = {};
+// Profile
+const educationSections = defineCollection({
+  type: 'data',
+  schema: z.object({
+    order: z.number().int().positive(),
+    timeIndication: z.string(),
+    title: z.string(),
+    subtitle: z.string().optional(),
+    description: z.string(),
+  }),
+});
+
+// Portfolio
+const portfolioCases = defineCollection({
+  type: 'content',
+  schema: ({ image }) =>
+    z.object({
+      coverImg: image(),
+      c2a: z.object({
+        prompt: z.string(),
+        url: z.string().url(),
+        faIcon: z.string(),
+      }),
+      title: z.string(),
+      subtitle: z.string(),
+    }),
+});
+
+export const collections = {
+  portfolioCases: portfolioCases,
+  educationSections: educationSections,
+};
