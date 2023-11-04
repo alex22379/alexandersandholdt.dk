22163b884e09f23554474c07cd86c559d0bbd750
Alexander <alexanderhis2201@gmail.com>
2023-11-04 19:48:40 +0100

    cotd (Commit Of The Day)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,21 @@ import { defineConfig } from 'astro/config';
 import purgecss from 'astro-purgecss';
 import sitemap from '@astrojs/sitemap';
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [purgecss(), sitemap(), mdx()],
+  integrations: [
+    purgecss({
+      content: [process.cwd() + '/src/**/*.{astro,html}'],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    }),
+    sitemap(),
+    mdx(),
+  ],
   site: 'https://www.alexandersandholdt.dk',
   trailingSlash: 'never',
   build: {
-    format: 'file'
-  }
+    format: 'file',
+  },
 });
