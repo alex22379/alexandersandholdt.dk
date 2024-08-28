19bbca2b730571ebee79a23aa04089301ec06ee8
Alexander <38869188+alex22379@users.noreply.github.com>
2024-08-28 20:40:08 +0200

    Intial v2 commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,9 @@
 import { defineConfig } from 'astro/config';
-import purgecss from 'astro-purgecss';
-import sitemap from '@astrojs/sitemap';
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.alexandersandholdt.dk',
-  integrations: [
-    purgecss({
-      content: [process.cwd() + '/src/**/*.{astro,html}'],
-      defaultExtractor: (content) => content.match(/[\w-/:.]+(?<!:)/g) || [],
-    }),
-    sitemap({
-      i18n: {
-        defaultLocale: 'da',
-        locales: {
-          da: 'da',
-          en: 'en',
-        },
-      },
-    }),
-    mdx(),
-  ],
-  /*trailingSlash: 'never',*/
-  build: {
-    format: 'file',
-  },
+  integrations: [mdx()],
+	site: 'https://alexandersandholdt.dk'
 });
--- src/scripts/isTouchEnabled.ts
+++
@@ -1,5 +0,0 @@
-// from https://www.geeksforgeeks.org/how-to-detect-touch-screen-device-using-javascript/
-
-export default function isTouchEnabled() {
-  return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
-}
