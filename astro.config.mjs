5de2bd2a19fbe9f9df43e1ecd268e20ea3efad11
Alexander <38869188+alex22379@users.noreply.github.com>
2024-08-29 22:15:27 +0200

    fixed trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), icon()],
-  site: 'https://alexandersandholdt.dk'
+  site: 'https://alexandersandholdt.dk',
+	trailingSlash: "never"
 });
